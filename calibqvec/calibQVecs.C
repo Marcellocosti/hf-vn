a0959f6d554e5d403a182bed120bacddac1f4156
marcellocosti <marcellodicostanzo00@gmail.com>
2026-05-22 12:03:11 +0200

    Improvements to Reduced Q-vec calibrations

--- calibqvec/calibQVecs.C
+++ calibqvec/calibQVecs.C
@@ -4,7 +4,26 @@
 #include "TH2D.h"
 #include "TH3F.h"
 #include "TMath.h"
-#include "runinfo_test.h"
+#include "TSystem.h"
+
+enum Detectors {
+  kFT0C = 0,
+  kFT0A,
+  kFT0M,
+  kFV0A,
+  kTPCPOS,
+  kTPCNEG,
+  kTPCALL,
+  kNDetectors
+};
+
+struct DetectorConfig {
+    std::string name;
+    std::string dirsuffix;
+    std::string ref;
+};
+
+using DetectorMap = std::array<DetectorConfig, kNDetectors>;
 
 void Recenter(TH2* h, std::vector<double>& corr){
     corr.push_back(h->GetMean(1));
@@ -74,11 +93,11 @@ TH2F* ProjQxQyCentDiff(TH3F* hQxQyCentUncor, float zmin, float zmax, TFile* c){
 }
 
 
-std::vector<double> fillCorrections(string detector, string fname, string dirname, string ref, int nmode){
-    TFile* fin = new TFile(Form("%s.root",fname.c_str()),"read");
-    TH3F* hQxQyCentUncor = (TH3F*)fin->Get(Form("%s/histQvec%sUncorV%d",dirname.c_str(),ref.c_str(),nmode));
+std::vector<double> fillCorrections(string outdir, TFile* infile, string detector, string dirname, string ref, int nmode){
 
-    TFile* c = new TFile(Form("/home/mdicosta/DMesonEsE/RedQCalib/LocalTest/debugQvecs_%s_commented.root",detector.c_str()), "recreate");
+    TH3F* hQxQyCentUncor = (TH3F*)infile->Get(Form("%s/histQvec%sUncorV%d",dirname.c_str(),ref.c_str(),nmode));
+
+    TFile* c = new TFile(Form("%s/debugQvecs_%s.root", outdir.c_str(), detector.c_str()), "recreate");
     const int nCentBins = 100;      // 1% centrality differential
     TH2F* hQvecUncor[nCentBins];
     std::vector<double> CorUncor;
@@ -87,94 +106,147 @@ std::vector<double> fillCorrections(string detector, string fname, string dirnam
         Recenter(hQvecUncor[i], CorUncor);
         Twist(hQvecUncor[i], CorUncor);
         Rescale(hQvecUncor[i], CorUncor);
+        // std::cout << "[CentBin " << i << "] ref = \"" << ref << "\", nmode = " << nmode << "] Recenter " << CorUncor[i*6 + 0] << ", " << CorUncor[i*6 + 1] << std::endl;
+        // std::cout << "[CentBin " << i << "] ref = \"" << ref << "\", nmode = " << nmode << "] Twist " << CorUncor[i*6 + 2] << ", " << CorUncor[i*6 + 3] << std::endl;
+        // std::cout << "[CentBin " << i << "] ref = \"" << ref << "\", nmode = " << nmode << "] Rescale " << CorUncor[i*6 + 4] << ", " << CorUncor[i*6 + 5] << std::endl;
+        // std::cout << std::endl;
     }
-    std::cout << "[ref = \"" << ref << "\", nmode = " << nmode << "] Recenter " << CorUncor[0] << ", " << CorUncor[1] << std::endl;
-    std::cout << "[ref = \"" << ref << "\", nmode = " << nmode << "] Twist " << CorUncor[2] << ", " << CorUncor[3] << std::endl;
-    std::cout << "[ref = \"" << ref << "\", nmode = " << nmode << "] Rescale " << CorUncor[4] << ", " << CorUncor[5] << std::endl;
-    std::cout << std::endl;
-    delete fin;
     c->Close();
     return CorUncor;
 }
 
-void makeHist(int runId, string ext1Id, string ext2Id, ULong64_t sor, ULong64_t eor, int vn){
+
+void getCorrections(TFile* inFile,
+                    std::string outDir,
+                    const DetectorMap& detMap,
+                    int run,
+                    int vn,
+                    TFile* corrFile,
+                    bool isEse) {
 
     int nCentBins = 100;
     int nDetectorBins = 10;
     int nCorrectionParams = 6;
 
-    TH3F* hCCDB = new TH3F("ccdb","",
-                           nCentBins,0,nCentBins,                          // cent
-                           nCorrectionParams,0-0.5,nCorrectionParams-0.5,  // const
-                           nDetectorBins,0-0.5,nDetectorBins-0.5);         // det
 
-    std::string inputFile = "/home/mdicosta/DMesonEsE/RedQCalib/LocalTest/AnalysisResultsTest";
-    std::cout << "Filling corrections for FT0C ... " << std::endl;
-    std::vector<double> QvecCorFT0C   = fillCorrections("FT0C", inputFile, "q-vectors-correction", "", vn);
-    std::cout << "Filling corrections for TPCPOS ... " << std::endl;
-    std::vector<double> QvecCorTPCPOS = fillCorrections("TPCPOS", inputFile, "q-vectors-correction", "RefA", vn);
-    std::cout << "Filling corrections for TPCNEG ... " << std::endl;
-    std::vector<double> QvecCorTPCNEG = fillCorrections("TPCNEG", inputFile, "q-vectors-correction", "RefB", vn);
-    std::cout << "Filling corrections for FV0A ... " << std::endl;
-    std::vector<double> QvecCorFV0A   = fillCorrections("FV0A", inputFile, "q-vectors-correction", "", vn);
-    std::cout << "Filling corrections for FT0A ... " << std::endl;
-    std::vector<double> QvecCorFT0A   = fillCorrections("FT0A", inputFile, "q-vectors-correction", "RefA", vn);
-    std::cout << "Filling corrections for FT0M ... " << std::endl;
-    std::vector<double> QvecCorFT0M   = fillCorrections("FT0M", inputFile, "q-vectors-correction", "RefB", vn);
-    std::cout << "Filling corrections for TPCALL ... " << std::endl;
-    std::vector<double> QvecCorTPCall = fillCorrections("TPCALL", inputFile, "q-vectors-correction", "", vn);
+    TH3F* hCCDB = new TH3F("ccdb", "",
+                           nCentBins, 0, nCentBins,                          // cent
+                           nCorrectionParams, 0-0.5, nCorrectionParams-0.5,  // const
+                           nDetectorBins, 0-0.5, nDetectorBins-0.5);         // det
 
-    for(int i=0;i<nCentBins;i++){
-        for(int j=0;j<nCorrectionParams;j++){
-            hCCDB->SetBinContent(i+1, j+1, 1, QvecCorFT0C.at(j + i*6));      // Recenter (2 pars), Twist (2 pars), Rescale (2 pars)
-            hCCDB->SetBinContent(i+1, j+1, 2, QvecCorFT0A.at(j + i*6));      // for each 1% centrality bin
-            hCCDB->SetBinContent(i+1, j+1, 3, QvecCorFT0M.at(j + i*6));
-            hCCDB->SetBinContent(i+1, j+1, 4, QvecCorFV0A.at(j + i*6));
-            hCCDB->SetBinContent(i+1, j+1, 5, QvecCorTPCPOS.at(j + i*6));
-            hCCDB->SetBinContent(i+1, j+1, 6, QvecCorTPCNEG.at(j + i*6));
-            hCCDB->SetBinContent(i+1, j+1, 7, QvecCorTPCall.at(j + i*6));
+    std::string histBaseDir = isEse ? "q-vectors-correction" : "q-vectors-correction_ScalarProd";
+    std::vector<double> QvecCorFT0C   = fillCorrections(outDir, inFile, detMap[kFT0C].name,   Form("%s%s", histBaseDir.c_str(), detMap[kFT0C].dirsuffix.c_str()),   detMap[kFT0C].ref,   vn);
+    std::vector<double> QvecCorFT0A   = fillCorrections(outDir, inFile, detMap[kFT0A].name,   Form("%s%s", histBaseDir.c_str(), detMap[kFT0A].dirsuffix.c_str()),   detMap[kFT0A].ref,   vn);
+    std::vector<double> QvecCorFT0M   = fillCorrections(outDir, inFile, detMap[kFT0M].name,   Form("%s%s", histBaseDir.c_str(), detMap[kFT0M].dirsuffix.c_str()),   detMap[kFT0M].ref,   vn);
+    std::vector<double> QvecCorFV0A   = fillCorrections(outDir, inFile, detMap[kFV0A].name,   Form("%s%s", histBaseDir.c_str(), detMap[kFV0A].dirsuffix.c_str()),   detMap[kFV0A].ref,   vn);
+    std::vector<double> QvecCorTPCPOS = fillCorrections(outDir, inFile, detMap[kTPCPOS].name, Form("%s%s", histBaseDir.c_str(), detMap[kTPCPOS].dirsuffix.c_str()), detMap[kTPCPOS].ref, vn);
+    std::vector<double> QvecCorTPCNEG = fillCorrections(outDir, inFile, detMap[kTPCNEG].name, Form("%s%s", histBaseDir.c_str(), detMap[kTPCNEG].dirsuffix.c_str()), detMap[kTPCNEG].ref, vn);
+    std::vector<double> QvecCorTPCall = fillCorrections(outDir, inFile, detMap[kTPCALL].name, Form("%s%s", histBaseDir.c_str(), detMap[kTPCALL].dirsuffix.c_str()), detMap[kTPCALL].ref, vn);
 
-            hCCDB->SetBinContent(i+1, j+1, 8, QvecCorTPCNEG.at(j + i*6)); //dummy
-            hCCDB->SetBinContent(i+1, j+1, 9, QvecCorTPCNEG.at(j + i*6)); //dummy
-            hCCDB->SetBinContent(i+1, j+1, 10, QvecCorTPCNEG.at(j + i*6)); //dummy
+    // Map your detector IDs to their corresponding data vector reference and enum index
+    struct DetConfig {
+        int id;
+        const std::vector<double>& data;
+    };
+    std::vector<DetConfig> detectors = {
+        {kFT0C,   QvecCorFT0C},   {kFT0A,   QvecCorFT0A},   {kFT0M,   QvecCorFT0M},
+        {kFV0A,   QvecCorFV0A},   {kTPCPOS, QvecCorTPCPOS}, {kTPCNEG, QvecCorTPCNEG},
+        {kTPCALL, QvecCorTPCall}
+    };
+
+    // Names of correction parameters matching your indexing order (0 to 5)
+    std::vector<std::string> paramNames = {"RecenterX", "RecenterY", "TwistX", "TwistY", "RescaleX", "RescaleY"};
+
+    // Setup 1D QA histograms programmatically 
+    std::map<int, std::vector<TH1F>> hParams;
+    for (const auto& det : detectors) {
+        for (const auto& name : paramNames) {
+            std::string hName = Form("hPar%s_%s", name.c_str(), detMap[det.id].name.c_str());
+            std::string hTitle = Form("%s - %s", name.c_str(), detMap[det.id].name.c_str());
+            hParams[det.id].emplace_back(hName.c_str(), hTitle.c_str(), nCentBins, 0, nCentBins);
         }
     }
 
-    // const string ccdbPath = "http://alice-ccdb.cern.ch";
-    // const string ccdbInternalPath = "Users/m/mdicosta/Qvector/Pass4/QvecCalib";
-    // o2::ccdb::CcdbApi ccdb;
-    // map<string, string> metadata;//, metadataRCT, header; // NOTE: Re-enable the other two if timing information is needed.
-    // ccdb.init(Form("%s", ccdbPath.data()));
-    // if(vn==2){
-    //     string harmonics = "v2";
-    //     metadata["runnum"] = std::to_string(runId);
-    //     metadata["harmonics"] = harmonics;
-    //     ccdb.storeAsTFileAny(hCCDB, Form("%s/%s", ccdbInternalPath.data(), harmonics.c_str()), metadata, sor, eor);
-    // }
+    // 2. Fill Histograms using streamlined loops
+    for (int i = 0; i < nCentBins; i++) {
+        for (int j = 0; j < nCorrectionParams; j++) {
+            int linearIndex = j + i * nCorrectionParams;
 
-    // if(vn==3){
-    // string harmonics = "v3";
-    // metadata["runnum"] = std::to_string(runId);
-    // metadata["harmonics"] = harmonics;
-    // ccdb.storeAsTFileAny(hCCDB, Form("%s/%s", ccdbInternalPath.data(), harmonics.c_str()), metadata, sor, eor);
-    // }
+            for (const auto& det : detectors) {
+                double val = det.data.at(linearIndex);
+                hCCDB->SetBinContent(i + 1, j + 1, det.id + 1, val);
+                hParams[det.id][j].SetBinContent(i + 1, val);
+            }
 
-    // if(vn==4){
-    //     string harmonics = "v4";
-    //     metadata["runnum"] = std::to_string(runId);
-    //     metadata["harmonics"] = harmonics;
-    //     ccdb.storeAsTFileAny(hCCDB, Form("%s/%s", ccdbInternalPath.data(), harmonics.c_str()), metadata, sor, eor);
-    // }
-    // // metadata.clear();
+            // Dummy detector bins (8, 9, 10) using TPCNEG data
+            for (int dummyDet = 8; dummyDet <= 10; dummyDet++) {
+                hCCDB->SetBinContent(i + 1, j + 1, dummyDet, QvecCorTPCNEG.at(linearIndex));
+            }
+        }
+    }
+
+    // 3. Create Directories and Write Histograms cleanly
+    corrFile->cd();
+    hCCDB->Write();
+
+    for (const auto& det : detectors) {
+        corrFile->mkdir(detMap[det.id].name.c_str());
+        corrFile->cd(detMap[det.id].name.c_str());
+
+        for (int j = 0; j < nCorrectionParams; j++) {
+            hParams[det.id][j].Write();
+        }
+    }
 }
 
-void calibQVecs(){
+void calibQVecs(std::string outDir,
+                std::string inFileDir,
+                int run,
+                std::string wagonSuffixFT0A,
+                std::string whichDetFT0A,
+                std::string wagonSuffixFT0C,
+                std::string whichDetFT0C,
+                std::string wagonSuffixFT0M,
+                std::string whichDetFT0M,
+                std::string wagonSuffixFV0A,
+                std::string whichDetFV0A,
+                std::string wagonSuffixTPCPOS,
+                std::string whichDetTPCPOS,
+                std::string wagonSuffixTPCNEG,
+                std::string whichDetTPCNEG,
+                std::string wagonSuffixTPCALL,
+                std::string whichDetTPCALL,
+                bool isEse) {
+
+    gErrorIgnoreLevel = kError;
+
+    std::string qVectType = isEse ? "EsE" : "SP";
+    gSystem->mkdir(Form("%s/runs/%d/%s/qvecCorrs/qvec_corrections.root", outDir.c_str(), run, qVectType.c_str()), true);
+
+    DetectorMap detMap = {{
+        {"FT0C",   wagonSuffixFT0C,   whichDetFT0C},
+        {"FT0A",   wagonSuffixFT0A,   whichDetFT0A},
+        {"FT0M",   wagonSuffixFT0M,   whichDetFT0M},
+        {"FV0A",   wagonSuffixFV0A,   whichDetFV0A},
+        {"TPCPOS", wagonSuffixTPCPOS, whichDetTPCPOS},
+        {"TPCNEG", wagonSuffixTPCNEG, whichDetTPCNEG},
+        {"TPCALL", wagonSuffixTPCALL, whichDetTPCALL}
+    }};
+
+    TFile* fileCorrs = new TFile(Form("%s/runs/%d/%s/qvecCorrs/qvec_corrections.root", outDir.c_str(), run, qVectType.c_str()), "recreate");
+    TFile* inputFile = new TFile(Form("%s/AnalysisResults_%d.root", inFileDir.c_str(), run), "read");
 
     std::array<int, 1> harmonics = {2}; // ,3,4};
     for (int vn : harmonics) {
-        for(int i=0;i<nrun;i++){
-            makeHist(runnums[i], Form("qvec_fit_%d",runnums[i]), Form("qvec_tpc_%d",runnums[i]), sors[i], eors[i], vn);
-        }
+        getCorrections(inputFile,
+                       Form("%s/runs/%d/%s/qvecCorrs/", outDir.c_str(), run, qVectType.c_str()),
+                       detMap,
+                       run,
+                       vn,
+                       fileCorrs,
+                       isEse);
     }
+
+    fileCorrs->Close();
 
 }
